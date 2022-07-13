65161ccdbeacf0437799281a02efbcb3c211376f
kramar-alexandr <kramar.alexandr@gmail.com.com>
2022-07-13 20:23:04 +0300

    Update stockMap.js

--- js/stockMap.js
+++ js/stockMap.js
@@ -110,6 +110,7 @@ function prepareData(data){//prepare data - add cell coordinates
 
 function getWarehouses(){ //Download warehouse structure data
   let edata;
+  console.log(window.location.protocol);
   if(window.location.protocol!=='file:'){//for real data remote request
       $.get("WebGetLocationsPlaces.hal", function( data ) {
         //$( ".result" ).html( data );
